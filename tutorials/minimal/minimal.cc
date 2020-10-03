6db89aee951f4fd1272d250cd0d55bb1b311844e
Chunpo Wang <cpwang@google.com>
2020-10-02 22:20:12 -0700

    Conversion back to Embree 2 in progress ...

--- tutorials/minimal/minimal.cc
+++ tutorials/minimal/minimal.cc
@@ -1,7 +1,7 @@
 // Copyright 2009-2020 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-#include <embree3/rtcore.h>
+#include <embree2/rtcore.h>
 #include <stdio.h>
 #include <math.h>
 #include <limits>
@@ -33,13 +33,24 @@
  * You should be able to compile this using a C or C++ compiler.
  */
 
+// TODO: Should we do the following from the readme.pdf?
+//#include <xmmintrin.h>
+//#include <pmmintrin.h>
+//...
+//_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+//_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+
 /* 
  * This is only required to make the tutorial compile even when
  * a custom namespace is set.
  */
-#if defined(RTC_NAMESPACE_OPEN)
-RTC_NAMESPACE_OPEN
-#endif
+//#if defined(RTC_NAMESPACE_OPEN)
+//RTC_NAMESPACE_OPEN
+//#endif
+
+// Copied from readme.pdf.
+struct Vertex { float x, y, z, a; };
+struct Triangle { int v0, v1, v2; };
 
 /*
  * We will register this error handler with the device in initializeDevice(),
@@ -68,9 +79,9 @@ RTCDevice initializeDevice()
   RTCDevice device = rtcNewDevice(NULL);
 
   if (!device)
-    printf("error %d: cannot create device\n", rtcGetDeviceError(NULL));
+    printf("error %d: cannot create device\n", rtcDeviceGetError(NULL));
 
-  rtcSetDeviceErrorFunction(device, errorFunction, NULL);
+  rtcDeviceSetErrorFunction2(device, errorFunction, NULL);
   return device;
 }
 
@@ -83,7 +94,7 @@ RTCDevice initializeDevice()
  */
 RTCScene initializeScene(RTCDevice device)
 {
-  RTCScene scene = rtcNewScene(device);
+  RTCScene scene = rtcDeviceNewScene(device, RTC_SCENE_DYNAMIC, RTC_INTERSECT1);
 
   /* 
    * Create a triangle mesh geometry, and initialize a single triangle.
@@ -95,137 +106,103 @@ RTCScene initializeScene(RTCDevice device)
    * to ensure proper alignment and padding. This is described in
    * more detail in the API documentation.
    */
-  RTCGeometry geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
-  float* vertices = (float*) rtcSetNewGeometryBuffer(geom,
-                                                     RTC_BUFFER_TYPE_VERTEX,
-                                                     0,
-                                                     RTC_FORMAT_FLOAT3,
-                                                     3*sizeof(float),
-                                                     3);
+  unsigned int mesh = rtcNewTriangleMesh(scene, RTC_GEOMETRY_STATIC, 1, 3);
 
-  unsigned* indices = (unsigned*) rtcSetNewGeometryBuffer(geom,
-                                                          RTC_BUFFER_TYPE_INDEX,
-                                                          0,
-                                                          RTC_FORMAT_UINT3,
-                                                          3*sizeof(unsigned),
-                                                          1);
+  Vertex* vertices = (Vertex*) rtcMapBuffer(scene, mesh, RTC_VERTEX_BUFFER);
+  vertices[0].x = 0.f; vertices[0].y = 0.f; vertices[0].z = 0.f;
+  vertices[1].x = 1.f; vertices[1].y = 0.f; vertices[1].z = 0.f;
+  vertices[2].x = 0.f; vertices[2].y = 1.f; vertices[2].z = 0.f;
+  rtcUnmapBuffer(scene, mesh, RTC_VERTEX_BUFFER);
 
-  if (vertices && indices)
-  {
-    vertices[0] = 0.f; vertices[1] = 0.f; vertices[2] = 0.f;
-    vertices[3] = 1.f; vertices[4] = 0.f; vertices[5] = 0.f;
-    vertices[6] = 0.f; vertices[7] = 1.f; vertices[8] = 0.f;
+  Triangle* triangles = (Triangle*) rtcMapBuffer(scene, mesh, RTC_INDEX_BUFFER);
+  triangles[0].v0 = 0; triangles[0].v1 = 1; triangles[0].v2 = 2;
+  rtcUnmapBuffer(scene, mesh, RTC_INDEX_BUFFER);
 
-    indices[0] = 0; indices[1] = 1; indices[2] = 2;
-  }
-
-  /*
-   * You must commit geometry objects when you are done setting them up,
-   * or you will not get any intersections.
-   */
-  rtcCommitGeometry(geom);
-
-  /*
-   * In rtcAttachGeometry(...), the scene takes ownership of the geom
-   * by increasing its reference count. This means that we don't have
-   * to hold on to the geom handle, and may release it. The geom object
-   * will be released automatically when the scene is destroyed.
-   *
-   * rtcAttachGeometry() returns a geometry ID. We could use this to
-   * identify intersected objects later on.
-   */
-  rtcAttachGeometry(scene, geom);
-  rtcReleaseGeometry(geom);
-
-  /*
-   * Like geometry objects, scenes must be committed. This lets
-   * Embree know that it may start building an acceleration structure.
-   */
-  rtcCommitScene(scene);
+  rtcCommit(scene);
 
   return scene;
 }
 
-/*
- * Cast a single ray with origin (ox, oy, oz) and direction
- * (dx, dy, dz).
- */
-void castRay(RTCScene scene, 
-             float ox, float oy, float oz,
-             float dx, float dy, float dz)
-{
-  /*
-   * The intersect context can be used to set intersection
-   * filters or flags, and it also contains the instance ID stack
-   * used in multi-level instancing.
-   */
-  struct RTCIntersectContext context;
-  rtcInitIntersectContext(&context);
-
-  /*
-   * The ray hit structure holds both the ray and the hit.
-   * The user must initialize it properly -- see API documentation
-   * for rtcIntersect1() for details.
-   */
-  struct RTCRayHit rayhit;
-  rayhit.ray.org_x = ox;
-  rayhit.ray.org_y = oy;
-  rayhit.ray.org_z = oz;
-  rayhit.ray.dir_x = dx;
-  rayhit.ray.dir_y = dy;
-  rayhit.ray.dir_z = dz;
-  rayhit.ray.tnear = 0;
-  rayhit.ray.tfar = std::numeric_limits<float>::infinity();
-  rayhit.ray.mask = -1;
-  rayhit.ray.flags = 0;
-  rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
-  rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
-
-  /*
-   * There are multiple variants of rtcIntersect. This one
-   * intersects a single ray with the scene.
-   */
-  rtcIntersect1(scene, &context, &rayhit);
-
-  printf("%f, %f, %f: ", ox, oy, oz);
-  if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID)
-  {
-    /* Note how geomID and primID identify the geometry we just hit.
-     * We could use them here to interpolate geometry information,
-     * compute shading, etc.
-     * Since there is only a single triangle in this scene, we will
-     * get geomID=0 / primID=0 for all hits.
-     * There is also instID, used for instancing. See
-     * the instancing tutorials for more information */
-    printf("Found intersection on geometry %d, primitive %d at tfar=%f\n", 
-           rayhit.hit.geomID,
-           rayhit.hit.primID,
-           rayhit.ray.tfar);
-  }
-  else
-    printf("Did not find any intersection.\n");
-}
-
-void waitForKeyPressedUnderWindows()
-{
-#if defined(_WIN32)
-  HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-  
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  if (!GetConsoleScreenBufferInfo(hStdOutput, &csbi)) {
-    printf("GetConsoleScreenBufferInfo failed: %d\n", GetLastError());
-    return;
-  }
-  
-  /* do not pause when running on a shell */
-  if (csbi.dwCursorPosition.X != 0 || csbi.dwCursorPosition.Y != 0)
-    return;
-  
-  /* only pause if running in separate console window. */
-  printf("\n\tPress any key to exit...\n");
-  int ch = getch();
-#endif
-}
+///*
+// * Cast a single ray with origin (ox, oy, oz) and direction
+// * (dx, dy, dz).
+// */
+//void castRay(RTCScene scene, 
+//             float ox, float oy, float oz,
+//             float dx, float dy, float dz)
+//{
+//  /*
+//   * The intersect context can be used to set intersection
+//   * filters or flags, and it also contains the instance ID stack
+//   * used in multi-level instancing.
+//   */
+//  struct RTCIntersectContext context;
+//  rtcInitIntersectContext(&context);
+//
+//  /*
+//   * The ray hit structure holds both the ray and the hit.
+//   * The user must initialize it properly -- see API documentation
+//   * for rtcIntersect1() for details.
+//   */
+//  struct RTCRayHit rayhit;
+//  rayhit.ray.org_x = ox;
+//  rayhit.ray.org_y = oy;
+//  rayhit.ray.org_z = oz;
+//  rayhit.ray.dir_x = dx;
+//  rayhit.ray.dir_y = dy;
+//  rayhit.ray.dir_z = dz;
+//  rayhit.ray.tnear = 0;
+//  rayhit.ray.tfar = std::numeric_limits<float>::infinity();
+//  rayhit.ray.mask = -1;
+//  rayhit.ray.flags = 0;
+//  rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+//  rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
+//
+//  /*
+//   * There are multiple variants of rtcIntersect. This one
+//   * intersects a single ray with the scene.
+//   */
+//  rtcIntersect1(scene, &context, &rayhit);
+//
+//  printf("%f, %f, %f: ", ox, oy, oz);
+//  if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID)
+//  {
+//    /* Note how geomID and primID identify the geometry we just hit.
+//     * We could use them here to interpolate geometry information,
+//     * compute shading, etc.
+//     * Since there is only a single triangle in this scene, we will
+//     * get geomID=0 / primID=0 for all hits.
+//     * There is also instID, used for instancing. See
+//     * the instancing tutorials for more information */
+//    printf("Found intersection on geometry %d, primitive %d at tfar=%f\n", 
+//           rayhit.hit.geomID,
+//           rayhit.hit.primID,
+//           rayhit.ray.tfar);
+//  }
+//  else
+//    printf("Did not find any intersection.\n");
+//}
+//
+//void waitForKeyPressedUnderWindows()
+//{
+//#if defined(_WIN32)
+//  HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+//  
+//  CONSOLE_SCREEN_BUFFER_INFO csbi;
+//  if (!GetConsoleScreenBufferInfo(hStdOutput, &csbi)) {
+//    printf("GetConsoleScreenBufferInfo failed: %d\n", GetLastError());
+//    return;
+//  }
+//  
+//  /* do not pause when running on a shell */
+//  if (csbi.dwCursorPosition.X != 0 || csbi.dwCursorPosition.Y != 0)
+//    return;
+//  
+//  /* only pause if running in separate console window. */
+//  printf("\n\tPress any key to exit...\n");
+//  int ch = getch();
+//#endif
+//}
 
 
 /* -------------------------------------------------------------------------- */
